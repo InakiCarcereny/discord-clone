3d91836257b249c504871079c4213bb2c6428623
inakicarcereny <inakicarce@hotmail.com.ar>
2024-10-08 21:03:04 -0300

    add server controller and model implementations for CRUD operations

---
+++ controllers/server.controllers.js
@@ -0,0 +1,9 @@
+export const getServer = async (req, res) => {};
+
+export const getServers = async (req, res) => {};
+
+export const createServer = async (req, res) => {};
+
+export const updateServer = async (req, res) => {};
+
+export const deleteServer = async (req, res) => {};
---
+++ models/server.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const serverSchema = new mongoose.Schema(
+  {
+    tittle: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    logo: {
+      data: Buffer,
+      contentType: String,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Server", serverSchema);
