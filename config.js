5c9718d34142674ade20450cbf3aa87047e5059b
inakicarcereny <inakicarce@hotmail.com.ar>
2024-10-08 21:03:21 -0300

    add cookie-parser and cors middleware; set up JWT_SECRET and SALT in config for future authentication features

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
 export const PORT = process.env.PORT || 5000;
+
+export const SALT = 10;
+
+export const JWT_SECRET = process.env.JWT_SECRET || "secret";
--- index.js
+++ index.js
@@ -2,14 +2,18 @@ import express from "express";
 import { PORT } from "./config.js";
 import { connectDB } from "./db.js";
 import userRoutes from "./routes/auth.routes.js";
+import serverRoutes from "./routes/server.routes.js";
+import cookieParser from "cookie-parser";
 
 connectDB();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/auth", userRoutes);
+app.use("/server", serverRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
