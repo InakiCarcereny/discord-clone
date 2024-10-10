840ca0a26caab5ec376b4575f3b2124e912d21ac
inakicarcereny <inakicarce@hotmail.com.ar>
2024-10-10 17:15:06 -0300

    add login page with header, form, and footer components; include Discord icon for branding

---
+++ client/src/app/(routes)/login/components/header/Header.login.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="font-semibold text-2xl text-white text-center">
+      Create account
+    </header>
+  );
+}
---
+++ client/src/app/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Home() {
+  return <span className="font-semibold text-xl">Hello discord clone</span>;
+}
