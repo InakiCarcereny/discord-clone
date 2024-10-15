e02dc91c73ec2fe81c672ecc11647480d9bd1b66
inakicarcereny <inakicarce@hotmail.com.ar>
2024-10-14 23:37:55 -0300

    refactor HomeLayout component; improve sidebar integration and authentication handling with new UI features and state management

--- client/src/app/(routes)/home/layout.tsx
+++ client/src/app/(routes)/home/layout.tsx
@@ -2,11 +2,18 @@
 
 import { Poppins } from "next/font/google";
 
-import { useEffect } from "react";
-import { useAuth } from "../register/context/auth.context";
+import { useEffect, useState } from "react";
+
 import { useRouter } from "next/navigation";
-import { Discord } from "@/app/icons/Discord";
 import { usePathname } from "next/navigation";
+
+import { useAuth } from "../register/context/auth.context";
+
+import { ServerSideBar } from "./components/server-side-bar/ServerSideBar.home";
+import { NavigateSideBar } from "./components/navigate-side-bar/NavigateSideBar.home";
+import { FriendsNav } from "./components/friends-nav/FriendsNav.home";
+import { User } from "./components/user/User.home";
+import { CreateServerModal } from "./components/create-server-modal/CreateServerModal.home";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -18,11 +25,16 @@ export default function HomeLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [open, setOpen] = useState(false);
+
   const { isAuthenticated, isLoading } = useAuth();
 
   const router = useRouter();
 
   const pathname = usePathname();
+
+  const showFriendsNav =
+    pathname.startsWith("/home") && !pathname.includes("/store");
 
   useEffect(() => {
     if (!isLoading) {
@@ -36,23 +48,41 @@ export default function HomeLayout({
     }
   }, [isAuthenticated, isLoading, router]);
 
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     <div
       className={`${poppins.className} ${poppins.className} antialiased flex h-screen w-screen bg-[#1c1d1f]`}
     >
-      <aside className="h-screen w-[100px] px-2 pt-4 flex flex-col items-center">
-        <span
-          className={`${
-            pathname === "/home" ? "rounded-[18px]" : ""
-          } bg-[#5865f2] flex flex-col items-center cursor-pointer rounded-3xl duration-200 px-2 py-2 hover:rounded-[18px]`}
+      <div className="flex">
+        <ServerSideBar open={open} handleOpen={handleOpen} />
+
+        <NavigateSideBar open={open} />
+      </div>
+
+      <div
+        className={`flex flex-col w-full bg-[#33353b] ${
+          open ? "blur-[2px]" : ""
+        }`}
+      >
+        {showFriendsNav && <FriendsNav />}
+
+        <main
+          className={`w-full h-full bg-[#33353b] px-10 ${
+            open ? "blur-[2px]" : ""
+          }`}
         >
-          <Discord className="text-white h-10 w-10" />
-        </span>
+          {children}
+        </main>
+      </div>
 
-        <div className="border border-zinc-600 rounded-full w-10 mt-2"></div>
-      </aside>
+      <div className="absolute bottom-0 left-[70px] z-50 bg-[#1e2024] w-[240px] h-[50px] flex items-center justify-between px-1 border-t border-[#33353b]">
+        <User />
+      </div>
 
-      <main className="w-full bg-[#33353b]">{children}</main>
+      {open && <CreateServerModal setOpen={setOpen} />}
     </div>
   );
 }
