a18380012f276cd124ceaed19c9128724965c554
inakicarcereny <inakicarce@hotmail.com.ar>
2024-10-17 21:46:30 -0300

    feat: enhance User context with user info retrieval and update functions; improve error handling and state management for user data

--- client/src/app/(routes)/home/context/user.context.tsx
+++ client/src/app/(routes)/home/context/user.context.tsx
@@ -1,21 +1,76 @@
-import { createContext, useContext, useEffect, useState } from "react";
+"use client";
 
-export const UserContext = createContext<undefined>(undefined);
+import {
+  getUserInfoRequest,
+  updataUserInfoRequest,
+} from "@/app/interceptors/user.interceptor";
+import { useContext, createContext, useState, useEffect } from "react";
+
+export interface UserContextTypes {
+  userInfo: User | null;
+  error: string[];
+  updateUserInfo: (data: User) => Promise<void>;
+}
+
+export interface User {
+  name: string;
+  pronouns: string;
+  about: string;
+  avatar: string;
+  poster: string;
+  primaryColor: string;
+  secondaryColor: string;
+}
+
+export const UserContext = createContext<UserContextTypes | undefined>(
+  undefined
+);
 
 export function useUser() {
   const context = useContext(UserContext);
 
-  if (context === undefined) {
+  if (context === undefined)
     throw new Error("useUser must be used within a UserProvider");
-  }
 
   return context;
 }
 
 export function UserProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
+  const [error, setError] = useState([]);
+
+  useEffect(() => {
+    async function getUserInfo() {
+      try {
+        const res = await getUserInfoRequest();
+        setUserInfo(res.data);
+      } catch (err: any) {
+        setError(err.response.data);
+      }
+    }
+    getUserInfo();
+  }, []);
+
+  const updateUserInfo = async (data: User) => {
+    try {
+      const res = await updataUserInfoRequest(data);
+      console.log(res);
+      setUserInfo(res.data);
+    } catch (err: any) {
+      console.log(err);
+      setError(err.response.data);
+    }
+  };
 
   return (
-    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+    <UserContext.Provider
+      value={{
+        userInfo,
+        error,
+        updateUserInfo,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
   );
 }
