0eb6c18d822a50f32a417cd370d4ede8c37c516a
inakicarcereny <inakicarce@hotmail.com.ar>
2024-10-24 18:09:18 -0300

    feat: add Dots and Rubbish SVG icons for enhanced UI flexibility across the application

---
+++ client/src/app/icons/Dots.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import type { SVGProps } from "react";
+
+export function Dots(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 20 20"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M14 10.25a1.25 1.25 0 1 1 2.5 0a1.25 1.25 0 0 1-2.5 0m-5 0a1.25 1.25 0 1 1 2.5 0a1.25 1.25 0 0 1-2.5 0m-5 0a1.249 1.249 0 1 1 2.5 0a1.25 1.25 0 1 1-2.5 0"
+      ></path>
+    </svg>
+  );
+}
